ccf9ab08649d3be4899c175c3de7e6a9f2d11aa6
Kazk12 <kazuko43KO@gmail.com>
2025-01-31 13:49:54 +0100

    Setup github

---
+++ src/Form/RecipeType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Recipe;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('description')
+            ->add('duration')
+            ->add('slug')
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
